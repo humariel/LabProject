f977b451a55699c3e07c9a22868f1cd744b7b2ae
humariel <hum4r13l@gmail.com>
2020-03-16 21:13:03 +0000

    kafka stuff. ainda nao esta nem tudo, nem bem. falta front end

--- src/main/java/Application/AppController.java
+++ src/main/java/Application/AppController.java
@@ -1,9 +1,19 @@
 package Application;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 @RestController
 public class AppController {
@@ -11,8 +21,45 @@ public class AppController {
     @GetMapping("/flights")
     public Flight[] flights(Model model){
         Flight[] flights = ScheduledTasks.getRecentFlights();
-        //model.addAttribute("flights", flights);
         return flights;
+    }
+
+    @GetMapping("/history")
+    public List<Flight> history(){
+        EntityManager em = Application.createEntityManager();
+        Query q = em.createQuery("select t from Flight t");
+        return q.getResultList();
+    }
+
+    @GetMapping("/consume")
+    public List<String> consume() {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("group.id", "test-group");
+
+        KafkaConsumer kafkaConsumer = new KafkaConsumer(properties);
+        List topics = new ArrayList();
+        topics.add("my-flights");
+        kafkaConsumer.subscribe(topics);
+
+        List<String> result = new ArrayList<>();
+        //boolean consuming = true;
+        try{
+            while(true) {
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(10);
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(String.format("Topic - %s, Partition - %d, Value: %s", record.topic(), record.partition(), record.value()));
+                    result.add(String.format("Topic - %s, Partition - %d, Value: %s", record.topic(), record.partition(), record.value()));
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            kafkaConsumer.close();
+        }
+        return result;
     }
 
 }
