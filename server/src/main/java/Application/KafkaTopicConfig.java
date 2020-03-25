b4c72b42d23f5b81589d89fba17a7a059d23ea6e
Enamak <gbmvitor@gmail.com>
2020-03-25 15:43:45 +0000

    Docker compose

--- server/src/main/java/Application/KafkaTopicConfig.java
+++ server/src/main/java/Application/KafkaTopicConfig.java
@@ -26,6 +26,6 @@ public class KafkaTopicConfig {
 
     @Bean
     public NewTopic flightsTopic() {
-        return new NewTopic("flights", 1, (short) 1);
+        return new NewTopic("flights", 2, (short) 1);
     }
 }
